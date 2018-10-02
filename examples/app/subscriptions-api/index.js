72e696f23e0965c5b246fc0ad459e781a79078b9
Yshayy <yshayy@gmail.com>
2018-10-02 20:44:04 +0300

    add implementation for several services

--- examples/app/subscriptions-api/index.js
+++ examples/app/subscriptions-api/index.js
@@ -1,48 +1,73 @@
-const Hapi=require('hapi');
-const Joi= require('joi');
+const app = require("express")();
+const bodyParser = require("body-parser");
+const Joi = require("joi");
+const asyncHandler = require("express-async-handler");
+const mongoist = require('mongoist');
+const validator = require("express-joi-validation")({});
+const nats = require('nats').connect({url: "nats://nats:4222"})
+const db = mongoist(process.env.MONGO_URL, {});
 
-const sgClient = require('@sendgrid/mail');
-sgClient.setApiKey(process.env.SENDGRID_API_KEY);
+app.use(bodyParser.json());
 
+app.post(
+  "/api/subscriptions",
+  validator.body(Joi.object({ 
+      source: Joi.object({
+          type: Joi.string().required().valid('twitter'),
+          feed: Joi.string().required()
+      }),
+      target: Joi.object({
+        type: Joi.string().required().valid('email'),
+        address: Joi.string().required()
+    })
+ })),
+  asyncHandler(async (req, res) => {  
+      await db.collection("subscriptions").insert({
+          ...req.body
+      });
+      nats.publish("new-subscriptions", {
+          ...req.body
+      });
+      res.sendStatus(200);
+  })
+);
 
-const server=Hapi.server({
-    port: process.env.PORT || 3000
+app.get(
+  "/api/subscriptions",
+  asyncHandler(async (req, res) => {
+     res.json(await db.collection("subscriptions").find());
+  })
+);
+
+app.delete(
+  "/api/subscriptions/:id",
+  validator.params({
+      id : Joi.string().required()
+  }),
+  asyncHandler(async (req, res) => {
+    await db.collection("subscriptions").remove({
+        _id: mongoist.ObjectId(req.params.id)
+    }, { justOne:true });
+    res.sendStatus(200);
+  })
+);
+
+app.get(
+  "/api/sources/:type",
+  validator.params({
+    type : Joi.string().required()
+  }),
+  asyncHandler(async (req, res) => {
+    const sources = await db.collection("subscriptions").find({
+        "source.type":req.params.type
+    },{
+        "source.feed": true
+    })
+
+    res.send(sources.map(x=>x.source.feed));
+  })
+);
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("subscriptions api is running")
 });
-
-const sender = process.env.SENDER_EMAIL;
-
-server.route({
-    method:'POST',
-    path:'/send',
-    handler: async ({payload},h) => {
-        const {message, title, to} = payload; 
-        await sgClient.send({
-            to,
-            from: sender,
-            subject: title,
-            text: message,
-            html: message,
-          });
-        return payload;
-    },
-    options: {
-        validate: {
-            payload: {
-                to: Joi.string().required(),
-                title: Joi.string().required(),
-                message: Joi.string().required()
-            }
-        }
-    }
-});
-
-(async function start() {
-    try {
-        await server.start();
-    }
-    catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    console.log('Server running at:', server.info.uri);
-})();
