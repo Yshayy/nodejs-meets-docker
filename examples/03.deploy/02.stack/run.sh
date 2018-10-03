f07deddbb4664397780d5ba895f0aabfc0e16834
Yshay Yaacobi <yshayy@gmail.com>
2018-10-03 08:12:59 +0300

    so fixes and improvements

---
+++ examples/03.deploy/02.stack/run.sh
@@ -0,0 +1,7 @@
+# docker-compose up --build
+
+# PWD
+# wget https://raw.githubusercontent.com/Yshayy/nodejs-meets-docker/master/examples/app/docker-compose.yml
+# export SENDGRID_API_KEY=***
+# docker-compose pull --parallel
+# docker stack deploy -c docker-compose.yml nodejs-docker
